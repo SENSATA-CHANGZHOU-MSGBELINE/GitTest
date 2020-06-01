7bed0855ccdf3558f40bc4adb1a22b1b21c02571
MorpheusPoi <43431360+MorpheusPoi@users.noreply.github.com>
2020-06-01 12:33:08 +0800

    ParticalCountProgram
    ParticalCountProgram

---
+++ ParticleCountProductsTracking/ParticleCountProductsTracking/ParticleCountDataTracking.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ParticleCountProductsTracking
+{
+    partial class ParticleCountDataTracking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbScanStatus = new System.Windows.Forms.Label();
+            this.lbReadResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboxDeviceID = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbScanStatus
+            // 
+            this.lbScanStatus.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbScanStatus.AutoSize = true;
+            this.lbScanStatus.Location = new System.Drawing.Point(633, 97);
+            this.lbScanStatus.Name = "lbScanStatus";
+            this.lbScanStatus.Size = new System.Drawing.Size(46, 17);
+            this.lbScanStatus.TabIndex = 1;
+            this.lbScanStatus.Text = "label1";
+            // 
+            // lbReadResult
+            // 
+            this.lbReadResult.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbReadResult.AutoSize = true;
+            this.lbReadResult.Location = new System.Drawing.Point(85, 97);
+            this.lbReadResult.Name = "lbReadResult";
+            this.lbReadResult.Size = new System.Drawing.Size(78, 17);
+            this.lbReadResult.TabIndex = 2;
+            this.lbReadResult.Text = "扫描结果：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "DeviceID:";
+            // 
+            // comboxDeviceID
+            // 
+            this.comboxDeviceID.DisplayMember = "DeviceID";
+            this.comboxDeviceID.FormattingEnabled = true;
+            this.comboxDeviceID.Location = new System.Drawing.Point(169, 51);
+            this.comboxDeviceID.Name = "comboxDeviceID";
+            this.comboxDeviceID.Size = new System.Drawing.Size(121, 24);
+            this.comboxDeviceID.TabIndex = 4;
+            // 
+            // ParticleCountDataTracking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 306);
+            this.Controls.Add(this.comboxDeviceID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbReadResult);
+            this.Controls.Add(this.lbScanStatus);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ParticleCountDataTracking";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ParticleCountDataTracking";
+            this.Load += new System.EventHandler(this.ParticleCountDataTracking_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbScanStatus;
+        private System.Windows.Forms.Label lbReadResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboxDeviceID;
+    }
+}
+
---
+++ ParticleCountProductsTracking/ParticleCountProductsTracking/ParticleCountDataTracking.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ParticleCountProductsTracking
+{
+    public partial class ParticleCountDataTracking : Form
+    {
+        SerialPort SerialPort;
+        public String ReadValue;
+
+        public ParticleCountDataTracking()
+        {
+            InitializeComponent();
+            InitPort();
+            DeviceIDFill();
+        }
+
+        void InitPort()
+        {
+            try
+            {
+                SerialPort = new SerialPort("COM4", 9600, Parity.None);
+                SerialPort.DataReceived += SerialPort_DataReceived;
+                SerialPort.ReceivedBytesThreshold = 1;
+                SerialPort.Encoding = Encoding.Default;
+                SerialPort.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("COM port open failed: " + e.Message);
+            }
+        }
+
+        void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(100);
+            byte[] data = new byte[SerialPort.BytesToRead];
+            SerialPort.Read(data, 0, SerialPort.BytesToRead);
+            ReadValue = Encoding.Default.GetString(data);
+
+
+
+            this.Invoke((EventHandler)delegate
+            {
+                if (lbReadResult.Text.Length < 5)
+                {
+                    lbReadResult.Text = "扫描结果：" + "请重新扫描！";
+                    lbScanStatus.Text = "NG";
+                    lbScanStatus.BackColor = Color.OrangeRed;
+                }
+                else
+                {
+                    if (true)
+                    {
+
+                    }
+                    lbReadResult.Text = "扫描结果：" + ReadValue;
+                    lbScanStatus.Text = "OK";
+                    lbScanStatus.BackColor = Color.LimeGreen;
+
+                }
+            });
+        }
+
+        //DatabaeCheck DatabaeCheck = new DatabaeCheck();
+
+        private void ParticleCountDataTracking_Load(object sender, EventArgs e)
+        {
+
+            lbScanStatus.Text = "Ready";
+
+
+        }
+
+
+        /// <summary>
+        /// 连接数据库，并读取DeviceID赋值给comBox
+        /// </summary>
+        private void DeviceIDFill()
+        {
+
+            string deviceid = "deviceid";
+            string database = "msgreportingcz.dbo.msgdeviceids";
+            string sqlServerInfo = "Data Source = MSGReportingCZ;Initial Catalog=MSGReportingCZ;Integrated Security=True";
+            string SqlStatement = "select " + deviceid + " from " + database;
+
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlServerInfo))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand(SqlStatement, sqlConnection);
+                    SqlDataReader dataReader = command.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+
+                        comboxDeviceID.Items.Add(dataReader["DeviceID"].ToString());
+                    }
+                    dataReader.Close();
+                }
+                catch (SqlException sqlE)
+                {
+
+                    MessageBox.Show("Data base connection error:" + sqlE.Message);
+
+                }
+            }
+        }
+    }
+}
